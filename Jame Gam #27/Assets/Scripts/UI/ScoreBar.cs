2325c7030e9100f0644ef3f45bf0df08bcf679d4
JoshOtter <87107050+JoshOtter@users.noreply.github.com>
2023-04-21 16:47:36 -0500

    Added basic timer and scorebar

---
+++ Jame Gam #27/Assets/Scripts/UI/ScoreBar.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider1;
+    [SerializeField] private Slider slider2;
+
+    [SerializeField] private float number1 = 100f;
+    [SerializeField] private float number2 = 100f;
+
+    void Start()
+    {
+        // Set the slider values to 0 initially
+        slider1.value = 0;
+        slider2.value = 0;
+    }
+
+    //void Update()
+    //{
+    //    UpdateSliderValues();
+    //}
+    #region TEST FUNCTIONS
+    public void IncreaseLeft()
+    {
+        number1 += 5;
+        UpdateSliderValues();
+    }
+
+    public void IncreaseRight()
+    {
+        number2 += 5;
+        UpdateSliderValues();
+    }
+
+    public void DecreaseLeft()
+    {
+        number1 -= 5;
+        UpdateSliderValues();
+    }
+
+    public void DecreaseRight()
+    {
+        number2 -= 5;
+        UpdateSliderValues();
+    }
+    #endregion
+
+    public void SetNumbers(float newNumber1, float newNumber2)
+    {
+        number1 = newNumber1;
+        number2 = newNumber2;
+        UpdateSliderValues();
+    }
+
+    private void UpdateSliderValues()
+    {
+        if (number1 == number2)
+        {
+            slider1.value = 0;
+            slider2.value = 0;
+        }
+        else if (number1 > number2)
+        {
+            slider1.value = (number1 - number2) / number1;
+            slider2.value = 0;
+        }
+        else
+        {
+            slider1.value = 0;
+            slider2.value = (number2 - number1) / number2;
+        }
+    }
+}
